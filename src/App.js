672fa7cefdfb51267144112c2b93bd3381c06033
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-02-02 17:57:44 -0300

    Adicionei Hello World

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 function App() {
   return (
+    //JSX (Class em js é feita pra criar classes | className é css)
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello <b>World</b> React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
